831e6f7b66315d85b25c6d6f0ff71f1ea924fd66
Mynor Lopez <mynlop15@gmail.com>
2017-06-08 10:37:56 -0600

    estructuras condicionales

---
+++ estructuras_condicionales.php
@@ -0,0 +1,24 @@
+<?php
+
+$color = 'black';
+// if
+if($color == 'black'){
+    echo 'Color is black';
+}elseif($color == 'white'){
+    echo 'Color is white';
+}else{
+    echo 'Color ...'
+}
+
+// switch
+switch($color){
+    case 'blue':
+        echo 'Color is blue'
+        break
+    case 'red':
+        echo 'Color is red'
+        break
+    default: 
+        echo 'Other'
+        break
+}
