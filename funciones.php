0e17b5dddbbade53511b208663ad585501f9fbdd
Mynor Lopez <mynlop15@gmail.com>
2017-06-08 10:22:40 -0600

    funciones

---
+++ funciones.php
@@ -0,0 +1,22 @@
+<?php
+
+function hello($name){
+    echo 'Hello' . $name;
+    echo '<br>';
+}
+
+hello('Juan');
+hello('Santiago')
+
+$x = 100;
+
+function sum($num1 , $num2){
+    $x = $a + $b;
+    return $x;
+}
+
+$c = sum(1,2);
+var_dump($c);
+
+var_dump($x);
+
