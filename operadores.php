b9a6727e6316b53cbcdad173273212b94ad5df8f
Mynor Lopez <mynlop15@gmail.com>
2017-06-07 21:21:21 -0600

    operadores

---
+++ operadores.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 1;
+$b = 2;
+
+// suma
+$c = $a + $b;
+
+// resta
+$c = $a - $b;
+
+// division
+$c = $a / $b;
+//convertir a entero
+//$c = (int)($a/$b);
+
+// multiplicacion
+$c = $a * $b;
+
+//modulo
+$c = $a % $b;
+
+// potencia
+$c = $a ** $b;
