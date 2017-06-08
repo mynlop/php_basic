c69a47f21d04ba1cf63b1fe2afc29b01c58dd873
Mynor Lopez <mynlop15@gmail.com>
2017-06-07 21:12:43 -0600

    arreglos

---
+++ arrays.php
@@ -0,0 +1,7 @@
+<?php
+
+$arrayVar = ['Red', 'Blue', 'Black'];
+$arrayVar = [0 => 'Red', 1 => 'Blue', 2 => 'Black']
+$arrayVar = ['color1' => 'Red', 'color2' => 'Blue', 3 => 'Black']
+
+var_dump($arrayVar);
