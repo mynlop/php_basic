a9d38e2cde01aee9c3bd68bffa6fb9f0ba39cecd
Mynor Lopez <mynlop15@gmail.com>
2017-06-07 21:01:10 -0600

    empezando curso, variables y tipos de caracteres

---
+++ variables.php
@@ -0,0 +1,25 @@
+<?php
+
+// int
+$intVar = 1;
+var_dump($intVar);
+
+// float
+$floatVar = 1.2;
+var_dump($floatVar);
+
+// cadena de caracteres
+$stringVar = 'Hola mundo';
+var_dump($stringVar);
+
+// bool
+$boolVar = true;
+var_dump($boolVar);
+
+// null
+$nullVar = null;
+var_dump($nullVar);
+
+// arreglos
+$arrayVar = ['valor1', 12, 1.3];
+var_dump($arrayVar);
