9528b97718ee7cb84f0ce7ae5708d34b569c0f26
Mynor Lopez <mynlop15@gmail.com>
2017-06-12 12:45:34 -0600

    constructor y destructor

--- poo/index.php
+++ poo/index.php
@@ -4,6 +4,17 @@ class Car{
     // public $propietario;
     private $propietario;
 
+    // constructor
+    public function __construct($propietario){
+        $this->propietario = $propietario;
+        echo 'Constructor<br>';
+    }
+
+    // destruct
+    public function __destruct(){
+        echo 'Destruct<br>';
+    }
+
     public function move(){
         echo 'moving<br>';
     }
@@ -19,12 +30,12 @@ class Car{
 
 echo 'Class car<br>';
 
-$car = new Car();
-$car2 = new Car();
+$car = new Car('Santiago');
+$car2 = new Car('Mynor');
 $car->move();
 
-$car->setPropietario('Santiago');
-$car2->setPropietario('Mynor');
+// $car->setPropietario('Santiago');
+// $car2->setPropietario('Mynor');
 // $car->propietario = 'Alex';
 // echo 'Owner: ' . $car->propietario;
 echo 'Propietario: ' . $car->getPropietario() .'<br>'; 
