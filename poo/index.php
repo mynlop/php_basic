a02b614a1b627996bfc3ed4e69c8cc9bca8f6290
Mynor Lopez <mynlop15@gmail.com>
2017-06-12 16:14:49 -0600

    propiedades staticos

--- poo/index.php
+++ poo/index.php
@@ -12,9 +12,14 @@ use Vehicles\Truck;
 echo 'Class car<br>';
 $car = new Car('Santiago');
 $car->move();
-echo 'Propietario: ' . $car->getPropietario() .'<br>'; 
 
-echo '<br>Class truck<br>';
-$truck = new Truck('Mynor', 'Pickup');
-$truck->move();
-echo 'Propietario: ' . $truck->getPropietario();
+echo '<br>Class truck 1<br>';
+$truck1 = new Truck('Mynor', 'Pickup');
+$truck1->move();
+
+echo 'Class truck 2<br>';
+$truck2 = new Truck('Mynor', 'Pickup');
+$truck2->move();
+
+// echo '<br> Total Trucks: ' . Truck::$count;
+echo '<br> Total Trucks: ' . Truck::getTotal();
--- poo/vehicles/Truck.php
+++ poo/vehicles/Truck.php
@@ -5,14 +5,21 @@ namespace Vehicles;
 require_once 'VehicleBase.php';
 
 class Truck extends VehicleBase{
+    // public static $count = 0;
+    private static $count = 0;
     private $type;
 
     public function __construct($propietario, $type){
         $this->type = $type;
         $this->propietario = $propietario;
+        self::$count++;
     }
 
     public function move(){
         echo 'Truck: ' . $this->type . ' moving<br>';
+    }
+
+    public static function getTotal(){
+        return self::$count;
     }
 }
